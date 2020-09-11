e5d0884cceea580a084d5bbb6ef08a93885a95d4
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-09-11 17:07:39 -0400

    Making sure files have been submitted

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid')
 const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 /* -------------------------------------------------------- */
